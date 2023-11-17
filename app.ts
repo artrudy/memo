4616b1b33dfa6b07bd63dc306af447f2edda3dd9
artёm rudoy <art.rudy@gmail.com>
2023-11-17 23:51:46 +0200

    buttons of menu and card created

--- app.ts
+++ app.ts
@@ -7,30 +7,6 @@
 // [x] next card always show front first
 // [ ] feedback after card was flipped
 
-import { getCurrentCard, isDone, nextCard } from "./review.model.js";
-
-function displayCurrentCard() {
-  const currentCard = getCurrentCard();
-
-  document.querySelector("#currentCard")?.classList.remove("card--flipped");
-  document.querySelector("#currentCard .card__front")!.textContent =
-    currentCard.front;
-  document.querySelector("#currentCard .card__back")!.textContent =
-    currentCard.back;
-}
-
-document.querySelector("#btnFlip")?.addEventListener("click", function () {
-  document.querySelector("#currentCard")?.classList.toggle("card--flipped");
+document.querySelector("#startPracticingBtn")?.addEventListener("click", () => {
+  window.location.href = "review.html";
 });
-
-document.querySelector("#btnNextCard")?.addEventListener("click", function () {
-  if (isDone()) {
-    document.querySelector("#reviewZone")!.innerHTML = "<h2>All done!</h2>";
-    return;
-  }
-
-  nextCard();
-  displayCurrentCard();
-});
-
-displayCurrentCard();
--- dist/app.js
+++ dist/app.js
@@ -1,4 +1,3 @@
-"use strict";
 // [x] array of cards
 // [x] card has front and back
 // [x] display first card
@@ -7,28 +6,7 @@
 // [x] when all cards reviewed show done message
 // [x] next card always show front first
 // [ ] feedback after card was flipped
-var _a, _b;
-exports.__esModule = true;
-var review_model_js_1 = require("./review.model.js");
-function displayCurrentCard() {
-    var _a;
-    var currentCard = review_model_js_1.getCurrentCard();
-    (_a = document.querySelector("#currentCard")) === null || _a === void 0 ? void 0 : _a.classList.remove("card--flipped");
-    document.querySelector("#currentCard .card__front").textContent =
-        currentCard.front;
-    document.querySelector("#currentCard .card__back").textContent =
-        currentCard.back;
-}
-(_a = document.querySelector("#btnFlip")) === null || _a === void 0 ? void 0 : _a.addEventListener("click", function () {
-    var _a;
-    (_a = document.querySelector("#currentCard")) === null || _a === void 0 ? void 0 : _a.classList.toggle("card--flipped");
+var _a;
+(_a = document.querySelector("#startPracticingBtn")) === null || _a === void 0 ? void 0 : _a.addEventListener("click", function () {
+    window.location.href = "review.html";
 });
-(_b = document.querySelector("#btnNextCard")) === null || _b === void 0 ? void 0 : _b.addEventListener("click", function () {
-    if (review_model_js_1.isDone()) {
-        document.querySelector("#reviewZone").innerHTML = "<h2>All done!</h2>";
-        return;
-    }
-    review_model_js_1.nextCard();
-    displayCurrentCard();
-});
-displayCurrentCard();
