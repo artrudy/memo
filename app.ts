f4f7b7edaa760b94fcad70e78ed8f1b406076323
artёm rudoy <art.rudy@gmail.com>
2023-11-19 20:53:24 +0200

    new card form finished

--- app.ts
+++ app.ts
@@ -11,54 +11,55 @@ document.querySelector("#startPracticingBtn")?.addEventListener("click", () => {
   window.location.href = "review.html";
 });
 
-function getNewCardData() {
-  const newCardFormHeader = document.createElement("div");
-  newCardFormHeader.innerText = "New card";
-  newCardFormHeader.id = "newCardFormHeaderId";
-
-  const emptyDiv = document.createElement("div");
-
-  const newCardForm = document.createElement("form");
-  newCardForm.id = "newForm";
-  newCardForm.className = "forms";
-
-  const fronSideInput = document.createElement("input");
-  fronSideInput.type = "text";
-  fronSideInput.name = "fron side";
-  fronSideInput.placeholder = "front side of the card";
-
-  const backSideInput = document.createElement("input");
-  backSideInput.type = "text";
-  backSideInput.name = "back side";
-  backSideInput.placeholder = "back side of the card";
-
-  newCardFormHeader.appendChild(emptyDiv);
-  newCardFormHeader.appendChild(newCardForm);
-  newCardFormHeader.appendChild(emptyDiv);
-
-  newCardForm.appendChild(fronSideInput);
-  newCardFormHeader.appendChild(emptyDiv);
-
-  newCardForm.appendChild(backSideInput);
-  newCardFormHeader.appendChild(emptyDiv);
-
-  document.querySelector("#mainMenu")?.appendChild(newCardFormHeader);
-
-  const subitNewCardBtn = document.createElement("button");
-  subitNewCardBtn.id = "subitNewCardBtnId";
-  subitNewCardBtn.classList.add("buttons");
-
-  subitNewCardBtn.innerText = "Sunbmit new card.";
-
-  newCardFormHeader.appendChild(subitNewCardBtn);
-
-  const buttongroup = document.querySelector("#buttongroup") as HTMLElement;
-  if (buttongroup) {
-    buttongroup.style.display = "none";
-  }
-}
-
 const getCardData = document.querySelector("#newCardBtn") as HTMLElement;
 if (getCardData) {
-  getCardData.addEventListener("click", getNewCardData());
+  getCardData.addEventListener("click", () => {
+    const newCardFormHeader = document.createElement("div");
+    newCardFormHeader.innerText = "New card";
+    newCardFormHeader.id = "newCardFormHeaderId";
+
+    const emptyDiv = document.createElement("div");
+
+    const newCardForm = document.createElement("form");
+    newCardForm.id = "newForm";
+    newCardForm.className = "forms";
+
+    const fronSideInput = document.createElement("input");
+    fronSideInput.type = "text";
+    fronSideInput.name = "fron side";
+    fronSideInput.placeholder = "front side of the card";
+
+    const backSideInput = document.createElement("input");
+    backSideInput.type = "text";
+    backSideInput.name = "back side";
+    backSideInput.placeholder = "back side of the card";
+
+    newCardFormHeader.appendChild(emptyDiv);
+    newCardFormHeader.appendChild(newCardForm);
+    newCardFormHeader.appendChild(emptyDiv);
+
+    newCardForm.appendChild(fronSideInput);
+    newCardFormHeader.appendChild(emptyDiv);
+
+    newCardForm.appendChild(backSideInput);
+    newCardFormHeader.appendChild(emptyDiv);
+
+    document.querySelector("#mainMenu")?.appendChild(newCardFormHeader);
+
+    const subitNewCardBtn = document.createElement("button");
+    subitNewCardBtn.id = "subitNewCardBtnId";
+    subitNewCardBtn.classList.add("buttons");
+    subitNewCardBtn.innerText = "Sunbmit new card.";
+    newCardFormHeader.appendChild(subitNewCardBtn);
+
+    subitNewCardBtn.addEventListener("click", () => {
+      newCardFormHeader.style.display = "none";
+      buttongroup.style.display = "flex";
+    });
+
+    const buttongroup = document.querySelector("#buttongroup") as HTMLElement;
+    if (buttongroup) {
+      buttongroup.style.display = "none";
+    }
+  });
 }
